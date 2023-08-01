66b7f4c71105eb6174c1867502cc16aa1252e5ff
trongbao123 <phambao11062001@gmail.com>
2023-08-01 08:38:24 +0700

    first commit

---
+++ src/util/setting/config.js
@@ -0,0 +1,5 @@
+export const DOMAIN = "https://airbnbnew.cybersoft.edu.vn/";
+export const TOKEN = "UserToken";
+export const TOKEN_CYBER =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwNiIsIkhldEhhblN0cmluZyI6IjE0LzA3LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY4OTI5MjgwMDAwMCIsIm5iZiI6MTY2MDE1MDgwMCwiZXhwIjoxNjg5NDQwNDAwfQ.nvrySbONO7THMJnLTWgEwiGszUF7VXjBUnn36QUuwsQ";
+export const USER_LOGIN = "USER_LOGIN";
